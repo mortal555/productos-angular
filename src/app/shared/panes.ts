9ee637267744d2b898c37b51093d927085d47297
mortal555 <martin.tepostillo@alumnos.udg.mx>
2019-09-17 23:36:31 -0500

    Se agrego objeto panes con datos

---
+++ src/app/shared/panes.ts
@@ -0,0 +1,261 @@
+export const Panes = [
+    {
+        nombre: 'concha',
+        precio: 6,
+        descripcion: 'Rico pan con cubierta de pasta dulce',
+        imagen: 'https://d36tnp772eyphs.cloudfront.net/blogs/2/2018/10/conchas-mexicanas-shutterstock_409664776.jpg',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'picon',
+        precio: 8,
+        descripcion: 'Rico pan con crujiente cubierta',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'rol de canela',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'lechuza',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'moño',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'chocolatin',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'cuerno',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'empanada',
+        precio: 8,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'calabaza',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'mantecada',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pan dulce'
+    },
+    {
+        nombre: 'pastel de 3 leches',
+        precio: 200,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'pastel de zanahoria',
+        precio: 300,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'pastel de chocolate',
+        precio: 350,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'pastel de vainilla',
+        precio: 150,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'pastel de queso',
+        precio: 350,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'pastel marmoleado',
+        precio: 200,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'pastel imposible',
+        precio: 350,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'pastel red velvet',
+        precio: 300,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'pastel de pigna',
+        precio: 300,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'pasteleria'
+    },
+    {
+        nombre: 'galletas de matequilla',
+        precio: 3,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'con chispas de chocolate',
+        precio: 5,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'galletas de animalitos',
+        precio: .5,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'galletas de limon',
+        precio: 4,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'canelitas',
+        precio: 3,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'galletas de avena',
+        precio: 5,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'galletas de jengibre',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'galletas de coco',
+        precio: 2.5,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'galletas de amaranto',
+        precio: 2,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'galletas de nuez',
+        precio: 5,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'galletas de mermelada',
+        precio: 8,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'galletas'
+    },
+    {
+        nombre: 'cono con crema',
+        precio: 10,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'hojaldre'
+    },
+    {
+        nombre: 'rehilete',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'hojaldre'
+    },
+    {
+        nombre: 'cuadro',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'hojaldre'
+    },
+    {
+        nombre: 'strudel',
+        precio: 10,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'hojaldre'
+    },
+    {
+        nombre: 'empanada de hojaldre',
+        precio: 10,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'hojaldre'
+    },
+    {
+        nombre: 'pay de hojaldre',
+        precio: 6,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'hojaldre'
+    },
+    {
+        nombre: 'oreja',
+        precio: 5,
+        descripcion: 'descripcion generica',
+        imagen: 'http://conlaszarpasenlamasa.es/wp-content/uploads/2011/11/club-pan.png',
+        categoria: 'hojaldre'
+    }   
+]
